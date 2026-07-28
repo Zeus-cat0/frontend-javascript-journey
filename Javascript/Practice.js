f0a4f032e9dd4eef9489ce205b92edc30566a1d1
Zeus-cat0 <ghimired439@gmail.com>
2026-07-28 10:21:42 +0545

    Learn JavaScript variables

---
+++ Javascript/Practice.js
@@ -0,0 +1,10 @@
+// // alert() is a built-in JavaScript function.
+// // It displays a popup message in the browser.
+// // "hello" is the text shown in the popup.
+// // The user must click "OK" to close it.
+
+// alert("hello");
+
+// alert('good job'); // with text good job
+
+ 
